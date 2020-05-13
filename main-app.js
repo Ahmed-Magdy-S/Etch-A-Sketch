f8b50d55c8f8a0758846660e4406e35c71812853
Ahmed Magdy Seleem <Ahmed.Magdy.S.19@Gmail.com>
2020-05-13 07:01:59 +0200

    Set up a “hover” effect

--- main-app.js
+++ main-app.js
@@ -4,10 +4,18 @@ document.body.appendChild(containerDiv);
 containerDiv.classList.add('container')
 
 
-
+//Create Grid Squares Divs
 function createGridSquaresDivs (gridSquaresNum) {
     for (let i = 0; i < gridSquaresNum; i++) {
         containerDiv.appendChild(document.createElement('div'));
     }
 }
 createGridSquaresDivs(16*16);
+
+//added Hover Effect
+let gridDivs = document.querySelectorAll('.container div')
+gridDivs.forEach(function(onegrid){
+onegrid.addEventListener('mouseover', function(){
+    onegrid.style.background = "red";
+})
+})
