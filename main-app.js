c310b9207723ad768c0b58984cf013d23467c915
Ahmed Magdy Seleem <Ahmed.Magdy.S.19@Gmail.com>
2020-05-20 05:43:27 +0200

    remain last step

--- main-app.js
+++ main-app.js
@@ -1,21 +1,73 @@
+//Generate Rabdom Color:
+function getRandomColor() {
+    return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+}
+let percent = 0;
+
+let randomColor = getRandomColor()
+//Darken Color
+const RGB_Linear_Shade = (p, c) => {
+    var i = parseInt,
+        r = Math.round,
+        [a, b, c, d] = c.split(","),
+        P = p < 0,
+        t = P ? 0 : 255 * p,
+        P = P ? 1 + p : 1 - p;
+
+    return "rgb" + (d ? "a(" : "(") + r(i(a[3] == "a" ? a.slice(5) : a.slice(4)) * P + t) + "," + r(i(b) * P + t) + "," + r(i(c) * P + t) + (d ? "," + d : ")");
+}
+
+
 //Create Div Container with class container
 let containerDiv = document.createElement('div');
 document.body.appendChild(containerDiv);
 containerDiv.classList.add('container')
 
+//added width to container div
+let containerWidth = 50;
+containerDiv.style.width = containerWidth + 'rem';
+
 
 //Create Grid Squares Divs
-function createGridSquaresDivs (gridSquaresNum) {
-    for (let i = 0; i < gridSquaresNum; i++) {
+let gridNumbers = 16;
+
+function createGridSquaresDivs(gridSquaresNum) {
+
+    for (let i = 0; i < gridSquaresNum * gridSquaresNum; i++) {
         containerDiv.appendChild(document.createElement('div'));
     }
-}
-createGridSquaresDivs(16*16);
+    //added Width & Height
+    let gridDivs = document.querySelectorAll('.container div')
+    gridDivs.forEach(function (oneGrid) {
+        oneGrid.style.width = containerWidth / gridSquaresNum + 'rem';
+        oneGrid.style.height = containerWidth / gridSquaresNum + 'rem';
+        // Added Hover Effect
+        oneGrid.addEventListener('mouseenter', function () {
 
-//added Hover Effect
-let gridDivs = document.querySelectorAll('.container div')
-gridDivs.forEach(function(onegrid){
-onegrid.addEventListener('mouseover', function(){
-    onegrid.style.background = "red";
-})
+            oneGrid.style.backgroundColor = RGB_Linear_Shade(percent, randomColor);
+
+        })
+
+
+    })
+}
+
+createGridSquaresDivs(gridNumbers);
+
+
+
+//New Game Button
+document.querySelector('.new-game').addEventListener('click', function () {
+    document.querySelectorAll('.container div').forEach(function (oneGrid) {
+        oneGrid.style.background = 'none';
+        percent = 0;
+        randomColor = getRandomColor()
+    })
+    let numbers = prompt("how many squares per side to make the new grid");
+    if (numbers !== null && numbers !== '' && typeof + numbers == 'number') {
+        containerDiv.innerHTML = '';
+        createGridSquaresDivs(+numbers);
+
+    }
+
 })
